58236473d7b4dda5b790777534da313330100b82
Elgun Asgar <elgunasgerov@gmail.com>
2023-01-18 15:26:37 -0500

    fix

--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -5,48 +5,50 @@ const User = require("../models/userModel");
 
 logRouter.get("/:_id/logs", async (req, res) => {
   const id = mongoose.Types.ObjectId(req.params._id);
-  const from = new Date(req.query.from);
-  const to = new Date(req.query.to);
+  // const from = new Date(req.query.from);
+  // const to = new Date(req.query.to);
+
+  const from = req.query.from;
+  const to = req.query.to;
   const limit = req.query.limit;
 
-  // console.log("id", id);
-  // console.log("from", from);
-  // console.log("to", to);
-  // console.log("limit", limit);
-
+  console.log("from: ", from);
+  console.log("to: ", to);
+  console.log("limit: ", limit);
   try {
     const user = await User.findOne({
       _id: id,
     });
 
     user.count = user.log.length;
+    const responseLogLength = limit || user.log.length;
+    let filteredUserLog;
 
-    let userLog;
-    const withQueries = !isNaN(Date.parse(from));
-    console.log("withqueries?", withQueries);
-    if (withQueries) {
-      userLog = user.log
-        .filter((elem) => elem.date >= from && elem.date <= to)
-        .slice(0, limit)
-        .map((elem) => {
-          return {
-            description: elem.description ? elem.description : "",
-            duration: elem.duration ? elem.duration : 0,
-            date: elem.date.toDateString(),
-          };
-        });
-    } else {
-      userLog = user.log.map((elem) => {
-        return {
-          description: elem.description ? elem.description : "",
-          duration: elem.duration ? elem.duration : 0,
-          date: elem.date.toDateString(),
-        };
-      });
+    if (from && to) {
+      filteredUserLog = user.log
+        .filter(
+          (elem) => elem.date >= new Date(from) && elem.date <= new Date(to)
+        )
+        .slice(0, responseLogLength);
+    } else if (from) {
+      filteredUserLog = user.log
+        .filter((elem) => elem.date >= new Date(from))
+        .slice(0, responseLogLength);
+    } else if (to) {
+      filteredUserLog = user.log
+        .filter((elem) => elem.date <= new Date(to))
+        .slice(0, responseLogLength);
     }
 
+    userLog = filteredUserLog.map((elem) => {
+      return {
+        description: elem.description ? elem.description : "",
+        duration: elem.duration ? elem.duration : 0,
+        date: elem.date.toDateString(),
+      };
+    });
     user.log = userLog;
-    console.log("user here", user);
+
     res.send(user);
   } catch (err) {
     res.status(400).json({ error: err });
